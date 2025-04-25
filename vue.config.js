902bd473a4fcfa11f30f9729be0d46e21887de50
板板 <747657490@qq.com>
2025-04-25 15:41:35 +0800

    finish-change

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path');
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/newsletter/'
+    : '/',
   css: {
     loaderOptions: {
       less: {
